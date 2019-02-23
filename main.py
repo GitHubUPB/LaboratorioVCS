81eb3ce5cf943be22ec698b9ba3a04f1ad5d03db
Maicol Bernal <maicol.bernal.2019@upb.edu.co>
2019-02-23 11:57:31 -0500

    Solucion

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import math
-b=int(input("ingresar el numero b"))
-d=int(input("ingrese el numero d"))
+a=int(input("ingresar el Dato A"))
+b=int(input("ingresar el Dato B"))
+c=int(input("ingresar el Dato C"))
 
-doble=(b*b)
-raiz=math.sqrt(d)
+d=(b*b-4*a*c)
 
-print("el cuadrado es :",doble)
-print("la raiz es:",raiz)
+if a!=o:
+    if d<0:
+       print("las raices son las imaginarias")
+    else:
+        x1=(-b+(math.sqrt(d)))/(2*a)
+        x2=(-b-(math.sqrt(d)))/(2*a)
+       print("x1 = "+str (x1)+"x2 = "+str(x2))
+else:
+    print("el coeficiente A deber ser diferente de cero")
